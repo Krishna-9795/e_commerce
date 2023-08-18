47306c686a87335fff9fd74cfe2b9813e920e87d
Krishna-9795 <krishna@cartoonmangos.com>
2023-08-18 11:09:10 +0530

    1

--- my_project/models.py
+++ my_project/models.py
@@ -4,14 +4,14 @@ db=SQLAlchemy()
 
 
 class user(db.Model):
-    user_id = db.Column(db.Integer , primary_key =True )
+    user_id = db.Column(db.Integer , primary_key =True ,unique=True)
     name = db.Column(db.String(80) , nullable= False)
     email = db.Column(db.String(80) , nullable= False, unique = True)
     password = db.Column(db.String(80))
     wallet_balance = db.Column(db.Integer)
     created_on = db.Column(db.DateTime , default= datetime.utcnow)
     phone = db.Column(db.BigInteger)
-    status = db.Column(db.Boolean , default = True)
+    #status = db.Column(db.Boolean , default = True)
     
 class product(db.Model):
     product_id=db.Column(db.Integer , primary_key =True )
@@ -19,35 +19,35 @@ class product(db.Model):
     description=db.Column(db.String(80))
     price=db.Column(db.Float, nullable=False)
     stock_quantity=db.Column(db.Integer, nullable=False)
-    category_id = db.Column(db.Integer, ForeignKey('categories.category_id'), nullable=False)
-    
+    category_id = db.Column(db.Integer, ForeignKey=True('categories.category_id'), nullable=False)
+    image_url=db.Column(db.String,nullable=False)
 class category(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
 class order_table(db.Model):
     order_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('users.user_id'), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey=True('users.user_id'), nullable=False)
     order_date = db.Column(db.DateTime, nullable=False)
     total_amount = db.Column(db.Float, nullable=False)
 
 class OrderItem(db.Model):
     order_item_id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, ForeignKey('orders.order_id'), nullable=False)
-    product_id = db.Column(db.Integer, ForeignKey('products.product_id'), nullable=False)
+    order_id = db.Column(db.Integer, ForeignKey=True('orders.order_id'), nullable=False)
+    product_id = db.Column(db.Integer, ForeignKey=True('products.product_id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     unit_price = db.Column(db.Float, nullable=False)
     
 class Payment(db.Model):
     payment_id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, ForeignKey('orders.order_id'), nullable=False)
+    order_id = db.Column(db.Integer, ForeignKey=True('orders.order_id'), nullable=False)
     payment_date = db.Column(db.DateTime, nullable=False)
     payment_amount = db.Column(db.Float, nullable=False)
     payment_status = db.Column(db.Boolean, nullable=False)
     
 class Review(db.Model):
     review_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('users.user_id'), nullable=False)
-    product_id = db.Column(db.Integer, ForeignKey('products.product_id'), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey=True('users.user_id'), nullable=False)
+    product_id = db.Column(db.Integer, ForeignKey=True('products.product_id'), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     review_text = db.Column(db.String)
