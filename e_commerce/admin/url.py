57c5ae30b40d3a7811cff5c0ceb27307c87205d1
Krishna-9795 <krishna@cartoonmangos.com>
2023-10-11 15:37:28 +0530

    5.1.4

--- e_commerce/admin/url.py
+++ e_commerce/admin/url.py
@@ -1,14 +1,15 @@
 
 #from e_commerce.utils import admin_token
+from e_commerce.admin import view 
+from e_commerce.product.view import create_product,get_product,update_product,delete_product,create_product_variant,get_product_variant,update_product_variant,delete_product_variant,create_category,get_category,update_category,delete_category,create_manufacturer,get_manufacturers,get_manufacturer,update_manufacturer,delete_manufacturer
+from e_commerce.addresses.view import address_data,update_address,get_address,delete_address,shipping_address_data,get_ship_address,update_shipping_address,delete_shipping_address
+from e_commerce.carts.view import create_cart,get_cart,update_cart,delete_cart,create_cart_item,get_cart_item,update_cart_item,delete_cart_item
+from e_commerce.orders.view import create_order,get_order,update_order,delete_order,create_order_item,get_order_item,update_order_item,delete_order_item
+from e_commerce.coupons.view import create_coupon,get_coupons,get_coupon,update_coupon,delete_coupon
+from e_commerce.payments.view import create_payment,get_payment,update_payment,delete_payment,create_payment_method,get_payment_method,update_payment_method,delete_payment_method,create_transaction,get_transaction,update_transaction,delete_transaction
+from e_commerce.user.view  import register,get_user,get_all_users,update_user,delete_user
+from e_commerce.review.view import create_review,get_review,get_review,update_review,delete_review
 
-
-
-
-from e_commerce.carts import view
-from e_commerce.review import view
-from e_commerce.payments import view
-from e_commerce.orders import view
-from e_commerce.coupons import view
 from flask import Blueprint
 
 admin_bp = Blueprint('admin', __name__)
@@ -36,365 +37,357 @@ def delete_admin(id):
     return view.delete_admin(id)
 
 
-from e_commerce.product import view
+
 
 @admin_bp.route('/admin/product/create',methods=['POST'])
 def admin_prod_create():
-    return view.create_product()
+    return create_product()
 
 @admin_bp.route('admin/products/retrieve/<int:product_id>', methods=['GET'])
 def get_products(product_id):
-    return view.get_product(product_id)
+    return get_product(product_id)
 
 # Update a Product
 @admin_bp.route('admin/products/update/<int:product_id>', methods=['PUT'])
 def update_products(product_id):
-    return view.update_product(product_id)
+    return update_product(product_id)
 
 # Delete a Product
 @admin_bp.route('/admin/products/delete/<int:product_id>', methods=['DELETE'])
 def delete_products(product_id):
-    return view.delete_product(product_id)
+    return delete_product(product_id)
 
 # Create a Product Variant
 @admin_bp.route('/admin/products/product-variants', methods=['POST'])
 def create_product_variants():
-    return view.create_product_variant()
+    return create_product_variant()
 
 
 # Retrieve a Product Variant
 @admin_bp.route('/admin/products/product-variants/retrieve/<int:variant_id>', methods=['GET'])
 def get_product_variants(variant_id):
-    return view.get_product_variant(variant_id)
+    return get_product_variant(variant_id)
 
 # Update a Product Variant
 @admin_bp.route('/admin/products/product-variants/update/<int:variant_id>', methods=['PUT'])
 def update_product_variants(variant_id):
-    return view.update_product_variant(variant_id)
+    return update_product_variant(variant_id)
 
 # Delete a Product Variant
 @admin_bp.route('/admin/products/product-variants/delete/<int:variant_id>', methods=['DELETE'])
-def delete_product_variant(variant_id):
-    return  view.delete_product_variant(variant_id)
-
+def delete_product_variant_route(variant_id):
+    return  delete_product_variant(variant_id)
 
 # Create a Category
 @admin_bp.route('/admin/products/categories/register', methods=['POST'])
 def create_categories():
-    return view.create_category()
+    return create_category()
 
 # Retrieve a Category by ID     
 @admin_bp.route('/admin/products/categories/retrieve/<int:category_id>', methods=['GET'])
 def get_categories(category_id):
-    return view.get_category(category_id)
+    return get_category(category_id)
 
 # Update a Category by ID
 @admin_bp.route('/admin/products/categories/update/<int:category_id>', methods=['PUT'])
 def update_category(category_id):
-    return view.update_category(category_id)
+    return update_category(category_id)
 
 # Delete a Category by ID
 @admin_bp.route('/admin/products/categories/delete/<int:category_id>', methods=['DELETE'])
 def delete_categories(category_id):
-    return view.delete_category(category_id)
+    return delete_category(category_id)
 
 
 # Create a Manufacturer
 @admin_bp.route('/admin/products/manufacturers/register', methods=['POST'])
 def create_manufacturers():
-    return view.create_manufacturer()
+    return create_manufacturer()
 
 # Retrieve all Manufacturers
 @admin_bp.route('/admin/products/manufacturers/retrieve', methods=['GET'])
-def get_manufacturers():
-    return view.get_manufacturers()
+def get_manufacturers_route():
+    return get_manufacturers()
 
 # Retrieve a Manufacturer by ID
 @admin_bp.route('/admin/manufacturers/<int:manufacturer_id>', methods=['GET'])
-def get_manufacturer(manufacturer_id):
-    return view.get_manufacturer(manufacturer_id)
+def get_manufacturer_route(manufacturer_id):
+    return get_manufacturer(manufacturer_id)
 
 # Update a Manufacturer by ID
 @admin_bp.route('/admin/manufacturers/update/<int:manufacturer_id>', methods=['PUT'])
-def update_manufacturer(manufacturer_id):
-    return view.update_manufacturer(manufacturer_id)
+def update_manufacturer_route(manufacturer_id):
+    return  update_manufacturer(manufacturer_id)
 
 # Delete a Manufacturer by ID
 @admin_bp.route('/admin/manufacturers/delete/<int:manufacturer_id>', methods=['DELETE'])
-def delete_manufacturer(manufacturer_id):
-    return view.delete_manufacturer(manufacturer_id)
+def delete_manufacturer_route(manufacturer_id):
+    return delete_manufacturer(manufacturer_id)
 
-
-from e_commerce.addresses import view
 
 @admin_bp.route('/admin/address/register',methods=['POST'])
 #@jwt_required()
 def addresses_data():
-    return view.address_data()
+    return address_data()
 
 @admin_bp.route('/admin/address/update/<int:address_id>',methods=['PUT']) 
 # @jwt_required()
 def update_addresses(address_id):
-    return view.update_address(address_id)
+    return update_address(address_id)
 
 # Retrieve address
 @admin_bp.route('/admin/address/get/<int:address_id>',methods=['get'])
 def gets_address(address_id):
-    return view.get_address(address_id) 
+    return get_address(address_id) 
 
 # Delete
 @admin_bp.route('/admin/address/delete/<int:address_id>', methods=['DELETE'])
 # @jwt_required()
 def delete_addresses(address_id):
-    return view.delete_address(address_id)
+    return delete_address(address_id)
 
 # Creating a shipping address
 @admin_bp.route('/admin/address/shipping_addresses',methods=['POST'])
 #  @jwt_required()
 def ship_addresses_data():
-    return view.shipping_address_data()
+    return shipping_address_data()
+
 
 # Retrieving a shipping address
 @admin_bp.route('/admin/shipping_address/get/<int:address_id>',methods=['get'])
-def gets_ship_address(address_id):
-    return view.get_ship_address(address_id) 
-
+def gets_ship_address_route(address_id):
+    return get_ship_address(address_id) 
 
 # Updating a shipping address
 @admin_bp.route('/admin/address/shipping_address/update/<int:address_id>',methods=['PUT']) 
 #   @jwt_required()
 def update_ship_addresses(address_id):
-    return view.update_shipping_address(address_id)
+    return update_shipping_address(address_id)
 
 # Deleting a shipping address
 @admin_bp.route('/admin/address/shipping_address/delete/<int:address_id>', methods=['DELETE'])
 #  @jwt_required()
-def delete_shipping_addresses(address_id):
-    return view.delete_shipping_address(address_id)
-
+def delete_shipping_address_route(address_id):
+    return delete_shipping_address(address_id)
 
 # Create a Cart
 @admin_bp.route('/admin/carts/create', methods=['POST'])
 def create_carts():
-    return view.create_cart()
+    return create_cart()
 
 # Retrieve a Cart by ID
 @admin_bp.route('/admin/carts/retrieve/<int:cart_id>', methods=['GET'])
-def get_carts(cart_id):
-    return view.get_cart(cart_id)
+def get_cart_route(cart_id):
+    return get_cart(cart_id)
 
 # Update a Cart by ID
 @admin_bp.route('/admin/carts/update/<int:cart_id>', methods=['PUT'])
 def update_carts(cart_id):
-    return view.update_cart(cart_id)
+    return update_cart(cart_id)
 
 # Delete a Cart by ID
 @admin_bp.route('/admin/carts/delete/<int:cart_id>', methods=['DELETE'])
 def delete_carts(cart_id):
-    return view.delete_cart(cart_id)
+    return delete_cart(cart_id)
 
 # Create a Cart Item
 @admin_bp.route('/admin/carts/cart_items/create', methods=['POST'])
 def create_cart_items():
-    return view.create_cart_item()
+    return create_cart_item()
 
 # Retrieve a Cart Item by ID
 @admin_bp.route('/admin/carts/cart_items/retrieve/<int:cart_item_id>', methods=['GET'])
 def get_cart_items(cart_item_id):
-    return view.get_cart_item(cart_item_id)
+    return get_cart_item(cart_item_id)
 
 # Update a Cart Item by ID
 @admin_bp.route('/admin/carts/cart_items/update/<int:cart_item_id>', methods=['PUT'])
 def update_cart_items(cart_item_id):
-    return view.update_cart_item(cart_item_id)
+    return update_cart_item(cart_item_id)
 
 # Delete a Cart Item by ID
 @admin_bp.route('/admin/carts/cart_items/delete/<int:cart_item_id>', methods=['DELETE'])
-def delete_cart_item(cart_item_id):
-    return view.delete_cart_item(cart_item_id)
+def delete_cart_item_route(cart_item_id):
+    return delete_cart_item(cart_item_id)
 
 
-from e_commerce.coupons import view
 
 # Create a Coupon
 @admin_bp.route('/admin/coupons/register', methods=['POST'])
-def create_coupon():
-    return view.create_coupon()
+def create_coupon_route():
+    return create_coupon()
 
 # Retrieve all Coupons
 @admin_bp.route('/admin/coupons/retrieve', methods=['GET'])
-def get_coupons():
-    return view.get_coupons()
+def get_coupons_route():
+    return get_coupons()
 
 
 # Retrieve a Coupon by ID
 @admin_bp.route('/admin/coupons/retrieve/<int:coupon_id>', methods=['GET'])
-def get_coupon():
-    return view.get_coupon()
+def get_coupon_route():
+    return get_coupon()
 
 # Update a Coupon by ID
 @admin_bp.route('/admin/coupons/update/<int:coupon_id>', methods=['PUT'])
-def update_coupon():
-    return view.update_coupon()
+def update_coupon_route():
+    return update_coupon()
 
 # Delete a Coupon by ID
 @admin_bp.route('/admin/coupons/delete/<int:coupon_id>', methods=['DELETE'])
-def delete_coupon():
-    return view.delete_coupon()
+def delete_coupon_route():
+    return delete_coupon()
 
 
 
-from e_commerce.orders import view
 
 # Create an Order 
 @admin_bp.route('/admin/orders/register', methods=['POST'])
-def create_orders():
-    return view.create_order()
+def create_orders_route():
+    return create_order()
 
 #Return an order by id
 @admin_bp.route('/admin/orders/retrieve/<int:order_id>', methods=['GET'])
-def get_orders(order_id):
-    return view.get_order(order_id)
+def get_orders_route(order_id):
+    return get_order(order_id)
 
 # Update an order 
 @admin_bp.route('/admin/orders/update/<int:order_id>', methods=['PUT'])
-def update_orders(order_id):
-    return view.update_order(order_id)
+def update_orders_route(order_id):
+    return update_order(order_id)
 
 # Delete an Order by ID
 @admin_bp.route('/admin/orders/delete/<int:order_id>', methods=['DELETE'])
-def delete_orders(order_id):
-    return view.delete_order(order_id)
+def delete_orders_route(order_id):
+    return delete_order(order_id)
 
 # Create an Order Item
 @admin_bp.route('/admin/order-items/register', methods=['POST'])
-def create_order_items():
-    return view.create_order_item()
+def create_order_items_route():
+    return create_order_item()
 
 # Retrieve an Order Item by ID
 @admin_bp.route('/admin/order-items/retrieve/<int:order_item_id>', methods=['GET'])
-def get_order_item(order_item_id):
-    return view.get_order_item(order_item_id)
+def get_order_item_route(order_item_id):
+    return get_order_item(order_item_id)
 
 # Update an Order Item by ID
 @admin_bp.route('/admin/order-items/update/<int:order_item_id>', methods=['PUT'])
-def update_order_item(order_item_id):
-    return view.update_order_item(order_item_id)
+def update_order_item_route(order_item_id):
+    return update_order_item(order_item_id)
 
 # Delete an Order Item by ID
 @admin_bp.route('/admin/order-items/delete/<int:order_item_id>', methods=['DELETE'])
-def delete_order_item(order_item_id):
-    return view.delete_order_item(order_item_id)
+def delete_order_item_route(order_item_id):
+    return delete_order_item(order_item_id)
 
-from e_commerce.payments import view
+
+
 # Create a Payment
 @admin_bp.route('/admin/payments/create', methods=['POST'])
-def create_payment():
-    return view.create_payment()
+def create_payment_route():
+    return create_payment()
 
 # Retrieve a Payment by ID
 @admin_bp.route('/admin/payments/retrieve/<int:payment_id>', methods=['GET'])
-def get_payment(payment_id):
-    return view.get_payment(payment_id)
+def get_payment_route(payment_id):
+    return get_payment(payment_id)
 # Update a Payment by ID
 @admin_bp.route('/admin/payments/update/<int:payment_id>', methods=['PUT'])
-def update_payments(payment_id):
-    view.update_payment(payment_id) 
+def update_payments_route(payment_id):
+    return update_payment(payment_id) 
 # Delete a Payment by ID
 @admin_bp.route('/admin/payments/delete/<int:payment_id>', methods=['DELETE'])
-def delete_payment(payment_id):
-    return view.delete_payment(payment_id)
+def delete_payment_route(payment_id):
+    return delete_payment(payment_id)
 
 # Create a Payment Method
 @admin_bp.route('/admin/payments/payment_methods/create', methods=['POST'])
 def create_payments_method():
-    return view.create_payment_method()
+    return create_payment_method()
 
 # Retrieve a Payment Method by ID
 @admin_bp.route('/admin/payments/payment_methods/retrieve/<int:payment_method_id>', methods=['GET'])
-def get_payment_method(payment_method_id):
-    return view.get_payment_method(payment_method_id)
+def get_payment_method_route(payment_method_id):
+    return get_payment_method(payment_method_id)
 
 # Update a Payment Method by ID
 @admin_bp.route('/admin/payments/payment_methods/update/<int:payment_method_id>', methods=['PUT'])
-def update_payment_method(payment_method_id):
-    return view.update_payment_method(payment_method_id)
+def update_payment_method_route(payment_method_id):
+    return update_payment_method(payment_method_id)
 # Delete a Payment Method by ID
 @admin_bp.route('/admin/payments/payment_methods/delete/<int:payment_method_id>', methods=['DELETE'])
-def delete_payment_method(payment_method_id):
-    return view.delete_payment_method(payment_method_id)
+def delete_payment_method_route(payment_method_id):
+    return delete_payment_method(payment_method_id)
 # Create a Transaction
 @admin_bp.route('/admin/payments/transactions', methods=['POST'])
-def create_transaction():
-    return view.create_transaction()
+def create_transaction_route():
+    return create_transaction()
 
 # Retrieve a Transaction by ID
 @admin_bp.route('/admin/payments/transactions/<int:transaction_id>', methods=['GET'])
-def get_transaction(transaction_id):
-    return view.get_transaction(transaction_id)
+def get_transaction_route(transaction_id):
+    return get_transaction(transaction_id)
 
 # Update a Transaction by ID
 @admin_bp.route('/admin/payments/transactions/<int:transaction_id>', methods=['PUT'])
-def update_transaction(transaction_id):
-    return view.update_transaction(transaction_id)
+def update_transaction_route(transaction_id):
+    return update_transaction(transaction_id)
 
 # Delete a Transaction by ID
 @admin_bp.route('/admin/payments/transactions/<int:transaction_id>', methods=['DELETE'])
-def delete_transaction(transaction_id):
-    return view.delete_transaction(transaction_id)
+def delete_transaction_route(transaction_id):
+    return delete_transaction(transaction_id)
 
-from e_commerce.review import view
 # Create a Review
 @admin_bp.route('/admin/reviews/register', methods=['POST'])
-def create_review():
-    return view.create_review()
+def create_review_route():
+    return create_review()
 
 # Retrieve all Reviews
 @admin_bp.route('/admin/reviews/retrieve', methods=['GET'])
-def get_reviews():
-    return view.get_review()
+def get_reviews_route():
+    return get_review()
 
 # Retrieve a Review by ID
 @admin_bp.route('/admin/reviews/retrieve/<int:review_id>', methods=['GET'])
-def get_review(review_id):
-    return view.get_review(review_id)
+def get_review_route(review_id):
+    return get_review(review_id)
 
 # Update a Review by ID
 @admin_bp.route('/admin/reviews/update/<int:review_id>', methods=['PUT'])
-def update_review(review_id):
-    return view.update_review(review_id)
+def update_review_route(review_id):
+    return update_review(review_id)
 
 # Delete a Review by ID
 @admin_bp.route('/admin/reviews/delete/<int:review_id>', methods=['DELETE'])
-def delete_review(review_id):
-    return view.delete_review(review_id)
+def delete_review_route(review_id):
+    return delete_review(review_id)
 
 
-from e_commerce.user import view
 # create a user
 @admin_bp.route('admin/user/register', methods=['POST'])
 def registers():
-        return view.register()
+        return register()
 
 # Getting the user details according to user_id
 @admin_bp.route('/admin/user/users/<int:user_id>', methods=['GET'])
 def get_user_route(user_id):
-    return view.get_user(user_id)
+    return get_user(user_id)
 
 # Getting the list of users
 @admin_bp.route('/admin/user/user_retrieval', methods=['GET'])
 def get_all_users_route():
-    return view.get_all_users()
-
+    return get_all_users()
 
 # Update
 @admin_bp.route('/admin/user/update/<int:user_id>', methods=['PUT'])
 def update_user_route(user_id):
-    return view.update_user(user_id)
+    return update_user(user_id)
 # Delete
 @admin_bp.route('/admin/user/delete/<int:user_id>', methods=['DELETE'])
 def delete_user_route(user_id):
-    return view.delete_user(user_id)
+    return delete_user(user_id)
 
 
 
--- e_commerce/user/url.py
+++ e_commerce/user/url.py
@@ -1,12 +1,13 @@
 from flask_jwt_extended import jwt_required
 from e_commerce.user import view
-from e_commerce.addresses import view
-from e_commerce.carts import view
+from e_commerce.product.view import get_product,get_all_products,get_product_variant
+from e_commerce.addresses.view import address_data,update_address,get_address,delete_address,shipping_address_data,get_ship_address,update_shipping_address,delete_shipping_address
+from e_commerce.carts.view import create_cart,get_cart,update_cart,delete_cart,create_cart_item,get_cart_item,update_cart_item,delete_cart_item
+from e_commerce.orders.view import create_order,get_order,create_order_item,get_order_item,update_order_item,delete_order_item
+from e_commerce.coupons.view import get_coupons,get_coupon
+from e_commerce.payments.view import create_payment,get_payment,get_payment_method,create_transaction,get_transaction
+from e_commerce.review.view import get_review,get_review,update_review,delete_review
 
-from e_commerce.orders import view
-from e_commerce.payments import view
-from e_commerce.product import view
-from e_commerce.review import view
 from flask import Blueprint
 
 user_bp = Blueprint('user', __name__)
@@ -47,196 +48,193 @@ def delete_user_route(user_id):
 
 
 @user_bp.route('/address/register',methods=['POST'])
-#@jwt_required()
+@jwt_required()
 def addresses_data():
-    return view.address_data()
+    return address_data()
     
 @user_bp.route('/user/address/update/<int:address_id>',methods=['PUT']) 
-# @jwt_required()
+@jwt_required()
 def update_addresses(address_id):
-    return view.update_address(address_id)
+    return update_address(address_id)
 
 # Retrieve address
 @user_bp.route('/user/address/get/<int:address_id>',methods=['get'])
 def gets_address(address_id):
-    return view.get_address(address_id) 
+    return get_address(address_id) 
 
 # Delete
 @user_bp.route('/user/address/delete/<int:address_id>', methods=['DELETE'])
 # @jwt_required()
 def delete_addresses(address_id):
-    return view.delete_address(address_id)
+    return delete_address(address_id)
 
-# Creating a shippping address
+# Creating a shipping address
 @user_bp.route('/user/address/shipping_addresses',methods=['POST'])
 #  @jwt_required()
 def ship_addresses_data():
-    return view.shipping_address_data()
+    return shipping_address_data()
 
 # Retrieving a shipping address
 @user_bp.route('/user/shipping_address/get/<int:address_id>',methods=['get'])
 def gets_ship_address(address_id):
-    return view.get_ship_address(address_id) 
+    return get_ship_address(address_id) 
 
 
 # Updating a shipping address
 @user_bp.route('/user/address/shipping_address/update/<int:address_id>',methods=['PUT']) 
 #   @jwt_required()
 def update_ship_addresses(address_id):
-    return view.update_shipping_address(address_id)
+    return update_shipping_address(address_id)
 
 # Deleting a shipping address
 @user_bp.route('/user/address/shipping_address/delete/<int:address_id>', methods=['DELETE'])
 #  @jwt_required()
 def delete_shipping_addresses(address_id):
-    return view.delete_shipping_address(address_id)
+    return delete_shipping_address(address_id)
 
 # Create a Cart
 @user_bp.route('/user/carts/create', methods=['POST'])
 def create_carts():
-    return view.create_cart()
+    return create_cart()
 
 # Retrieve a Cart by ID
 @user_bp.route('/user/carts/retrieve/<int:cart_id>', methods=['GET'])
 def get_carts(cart_id):
-    return view.get_cart(cart_id)
+    return get_cart(cart_id)
 
 # Update a Cart by ID
 @user_bp.route('/user/carts/update/<int:cart_id>', methods=['PUT'])
 def update_carts(cart_id):
-    return view.update_cart(cart_id)
+    return update_cart(cart_id)
 
 # Delete a Cart by ID
 @user_bp.route('/user/carts/delete/<int:cart_id>', methods=['DELETE'])
 def delete_carts(cart_id):
-    return view.delete_cart(cart_id)
+    return delete_cart(cart_id)
 
 # Create a Cart Item
 @user_bp.route('/user/carts/cart_items/create', methods=['POST'])
 def create_cart_items():
-    return view.create_cart_item()
+    return create_cart_item()
 
 # Retrieve a Cart Item by ID
 @user_bp.route('/user/carts/cart_items/retrieve/<int:cart_item_id>', methods=['GET'])
 def get_cart_items(cart_item_id):
-    return view.get_cart_item(cart_item_id)
+    return  get_cart_item(cart_item_id)
 
 # Update a Cart Item by ID
 @user_bp.route('/user/carts/cart_items/update/<int:cart_item_id>', methods=['PUT'])
 def update_cart_items(cart_item_id):
-    return view.update_cart_item(cart_item_id)
+    return update_cart_item(cart_item_id)
 
 # Delete a Cart Item by ID
 @user_bp.route('/user/carts/cart_items/delete/<int:cart_item_id>', methods=['DELETE'])
 def delete_cart_item(cart_item_id):
-    return view.delete_cart_item(cart_item_id)
+    return delete_cart_item(cart_item_id)
 
 from e_commerce.coupons import view
 # Retrieve all Coupons
 @user_bp.route('user/coupons/retrieve', methods=['GET'])
 def get_coupons():
-    return view.get_coupons()
+    return  get_coupons()
 
 
 # Retrieve a Coupon by ID
 @user_bp.route('user/coupons/retrieve/<int:coupon_id>', methods=['GET'])
 def get_coupon(coupon_id):
-    return view.get_coupon(coupon_id)
+    return get_coupon(coupon_id)
 
 
 
 # Retrieve all Reviews
 @user_bp.route('/user/reviews/retrieve', methods=['GET'])
 def get_reviews():
-    return view.get_review()
+    return get_review()
 
 # Retrieve a Review by ID
 @user_bp.route('/user/reviews/retrieve/<int:review_id>', methods=['GET'])
 def get_review(review_id):
-    return view.get_review(review_id)
+    return get_review(review_id)
 
 # Update a Review by ID
 @user_bp.route('/user/reviews/update/<int:review_id>', methods=['PUT'])
 def update_review(review_id):
-    return view.update_review(review_id)
+    return update_review(review_id)
 
 # Delete a Review by ID
 @user_bp.route('/user/reviews/delete/<int:review_id>', methods=['DELETE'])
 def delete_review(review_id):
-    return view.delete_review(review_id)
+    return delete_review(review_id)
 
 from e_commerce.product import view
 # Retrieve a Product
 @user_bp.route('/user/products/retrieve/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    return view.get_product(product_id)
+    return get_product(product_id)
 # Gets all the products
 @user_bp.route('/user/products/retrieve', methods=['GET'])
 def get_products():
-    return view.get_all_products()
+    return get_all_products()
 
 # Retrieve all Product Variant
 @user_bp.route('/user/products/product-variants/retrieve>', methods=['GET'])
 def get_all_product_variants():
-    return view.get_all_product_variants()
+    return get_all_product_variants()
 
 # Retrieve a Product Variant
 @user_bp.route('/user/user/products/product-variants/retrieve/<int:variant_id>', methods=['GET'])
 def get_product_variants(variant_id):
-    return view.get_product_variant(variant_id)
+    return get_product_variant(variant_id)
 
 from e_commerce.payments import view
 # Create a Payment
 @user_bp.route('/user/payments/create', methods=['POST'])
 def create_payment():
-    return view.create_payment()
+    return create_payment()
 # Retrieve a Payment by ID
 @user_bp.route('/user/payments/retrieve/<int:payment_id>', methods=['GET'])
 def get_payment(payment_id):
-    return view.get_payment(payment_id)
+    return get_payment(payment_id)
 # Retrieve a Payment Method by ID
 @user_bp.route('/user/payments/payment_methods/retrieve/<int:payment_method_id>', methods=['GET'])
 def get_payment_method(payment_method_id):
-    return view.get_payment_method(payment_method_id)
+    return get_payment_method(payment_method_id)
 @user_bp.route('/user/payments/transactions', methods=['POST'])
 def create_transaction():
-    return view.create_transaction()
+    return create_transaction()
 
 # Retrieve a Transaction by ID
 @user_bp.route('/user/payments/transactions/<int:transaction_id>', methods=['GET'])
 def get_transaction(transaction_id):
-    return view.get_transaction(transaction_id)
+    return get_transaction(transaction_id)
 
 
 # Create an Order 
 @user_bp.route('/user/orders/register', methods=['POST'])
 def create_orders():
-    return view.create_order()
+    return create_order()
 
 #Return an order by id
 @user_bp.route('/user/orders/retrieve/<int:order_id>', methods=['GET'])
 def get_orders(order_id):
-    return view.get_order(order_id)
-
-
-
+    return get_order(order_id)
 
 # Create an Order Item
 @user_bp.route('/user/order-items/register', methods=['POST'])
 def create_order_items():
-    return view.create_order_item()
+    return create_order_item()
 
 # Retrieve an Order Item by ID
 @user_bp.route('/user/order-items/retrieve/<int:order_item_id>', methods=['GET'])
 def get_order_item(order_item_id):
-    return view.get_order_item(order_item_id)
+    return get_order_item(order_item_id)
 
 # Update an Order Item by ID
 @user_bp.route('/user/order-items/update/<int:order_item_id>', methods=['PUT'])
 def update_order_item(order_item_id):
-    return view.update_order_item(order_item_id)
+    return update_order_item(order_item_id)
 
 # Delete an Order Item by ID
 @user_bp.route('/user/order-items/delete/<int:order_item_id>', methods=['DELETE'])
 def delete_order_item(order_item_id):
-    return view.delete_order_item(order_item_id)
+    return delete_order_item(order_item_id)
